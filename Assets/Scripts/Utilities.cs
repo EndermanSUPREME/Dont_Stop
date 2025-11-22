e98a3b362f6c4b5e0b9da470ef45bb5ce136a550
Ender <nallahabi2003@gmail.com>
2025-11-21 20:29:22 -0500

    Added utilities for helpful async functionality

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,63 @@
+namespace Utilities
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using UnityEngine;
+
+    // custom sleep object that can be used to help with sleep operations
+    public class Sleep
+    {
+        float timeTarget = 0;
+        float timeElapsed = 0;
+
+        public Sleep(){}
+        public Sleep(float t) { timeTarget = t; _ = Update(); }
+        async Task Update()
+        {
+            while (timeElapsed < timeTarget)
+            {
+                if (!PlayerManager.Instance.gamePaused)
+                {
+                    timeElapsed += Time.deltaTime;
+                }
+                await Task.Yield();
+            }
+        }
+        public bool Finished() => timeElapsed >= timeTarget;
+    }
+
+    public class RunAfter
+    {
+        float timeTarget = 0;
+        float timeElapsed = 0;
+        Action func;
+        bool finished;
+
+        public RunAfter(){}
+        public RunAfter(float t, Action targetFunction)
+        {
+            timeTarget = t;
+            finished = false;
+            func = targetFunction;
+            _ = Update();
+        }
+
+        async Task Update()
+        {
+            while (timeElapsed < timeTarget)
+            {
+                if (!PlayerManager.Instance.gamePaused)
+                {
+                    timeElapsed += Time.deltaTime;
+                }
+                await Task.Yield();
+            }
+            Run();
+            finished = true;
+        }
+        // execute functions that do not return anything
+        void Run() => func();
+        public bool Finished() => finished;
+    }
+}
